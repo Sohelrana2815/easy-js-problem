173ed0750d279544ad71c72f391c636f25dad44b
Sohelrana2815 <sohel152302@gmail.com>
2024-03-01 10:43:09 +0600

    string

---
+++ api/string-info.js
@@ -0,0 +1,28 @@
+const first = "sohel";
+const last = "rana";
+const greet = "hello";
+const name = first + "" + last + " " + greet;
+// console.log(name);
+
+const a = 10;
+const b = 20;
+const result = " The sum of " + a + " and " + b + " is " + (a + b);
+// console.log(result);
+
+const math = `The sum of ${a}  and ${b} is ${a + b}`;
+// console.log(math);
+
+const email = `My name is SohelRana
+I'm 20 years old. 
+this is my programming course 
+and i love programming
+`;
+console.log(email);
+
+const numbers = [23, 52, 61, 64];
+const student = {
+  name: "SohelRana",
+  age: 23,
+};
+const math1 = `The sum of ${numbers[0]}  and ${student.age} is ${a + b}`;
+console.log(math1);
