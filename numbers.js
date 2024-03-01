8e95884d83663aed80f630450078999d20345331
Sohelrana2815 <sohel152302@gmail.com>
2024-03-01 09:22:48 +0600

    first commit

---
+++ numbers.js
@@ -0,0 +1,25 @@
+/**
+ * NaN is not a Number
+ */
+var applePrice = 29;
+var orangePrice = parseFloat("78.9");
+var orangePrice = parseInt("78.9");
+var candyPrice = 137.5;
+// var result = applePrice + orangePrice + candyPrice;
+// console.log(typeof result);
+// console.log(applePrice + orangePrice + candyPrice);
+// console.log(typeof orangePrice);
+console.log(orangePrice);
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+console.log(total);
+console.log(total.toFixed(5));
+const num = 50 / 0;
+console.log(num);
+const num1 = -50 / 0;
+console.log(num1);
+
+// parseInt(Convert to integer number)
+// parseFloat (convert to float number)
---
+++ operators.js
@@ -0,0 +1,30 @@
+var eggPrice = 10;
+var breadPrice = 54;
+var result = eggPrice + breadPrice;
+console.log(result);
+
+var sub = breadPrice - eggPrice;
+console.log(sub);
+
+var multiply = eggPrice * breadPrice;
+console.log(multiply);
+var divide = breadPrice / eggPrice;
+console.log(divide);
+
+var num1 = 10;
+var num2 = 4;
+var vag = num1 % num2;
+console.log(vag); // Reminder
+console.log(10 % 3);
+
+var money = 500;
+money = 550;
+
+var price = 32;
+price = price + 8;
+console.log(price);
+console.log(money);
+
+var newPrice = price + 10;
+var discountedPrice = newPrice - 20;
+console.log(discountedPrice);
