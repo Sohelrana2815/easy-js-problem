eb50fb7ceb7bdfa3aca2fd071f942de0bd73ece5
Sohelrana2815 <sohel152302@gmail.com>
2024-03-06 20:41:25 +0600

    react

---
+++ js for react/es6.js
@@ -0,0 +1,9 @@
+const numbers = [89, 13, 51, 32];
+const student = {
+  name: "SohelRana",
+  age: 32,
+  movies: ["king khan", "Dhakar mastan"],
+};
+
+const about = `My Name is ${student.name}`;
+console.log(about);
